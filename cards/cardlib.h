5d7a46da55fe51972589545a71fe741aea4e3123
Muath Khalifa <cy014385@student.reading.ac.uk>
2022-03-11 12:33:36 +0000

    Adding cards

---
+++ cards/cardlib.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+using namespace std;
+
+enum Suit { hearts, clubs, diamonds, spades };      // define suits
+
+struct aCard {                          // defines a card
+    int cardVal;                        // number 1..13
+    Suit cardSuit;                      // suit
+};
+
+aCard getCard(std::string stdno);        // declares function to get a card from stdno string
